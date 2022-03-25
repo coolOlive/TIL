f968af47d62749616d3a40a7419aa004f34dc623
coolOlive <galilee155@naver.com>
2022-03-25 22:33:51 +0900

    220325_백준[2750], 파이썬

---
+++ 코딩테스트 공부/220325_백준[2750].py
@@ -0,0 +1,12 @@
+# 버블정렬
+n=int(input())
+num=[int(input()) for i in range(n)]
+
+def bubble(num):
+    for i in range(n-1):
+        for j in range(n-1-i):
+            if num[j]>num[j+1]:
+                num[j],num[j+1]=num[j+1],num[j]
+
+bubble(num)
+[print(a) for a in num]
