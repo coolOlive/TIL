77149d685752c06e2a16b169d4c1d6fa83f1b176
coolOlive <galilee155@naver.com>
2022-03-30 14:10:36 +0900

    220330_백준[2775], 파이썬

---
+++ 코딩테스트 공부/220330_백준[2775].py
@@ -0,0 +1,12 @@
+# 기본수학1_부녀회장이 될테야
+
+t=int(input())
+
+for _ in range(t):
+    k=int(input())
+    n=int(input())
+    p_num=[i for i in range(1,n+1)]
+    for i in range(k):
+        for j in range(1,n):
+            p_num[j]+=p_num[j-1]
+    print(p_num[-1])
