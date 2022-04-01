0aea676c841aaa5fee76aaf2c7ce81d8ed9402b1
coolOlive <galilee155@naver.com>
2022-04-01 18:56:29 +0900

    220401_백준[2798], 파이썬

---
+++ 코딩테스트 공부/220401_백준[2798].py
@@ -0,0 +1,18 @@
+import sys
+input=sys.stdin.readline
+
+# 브루트 포스_블랙잭
+
+n,m=map(int,input().split())
+black=list(map(int,input().split()))
+balck_len = len(black)
+ans = 0
+for i in range(0, balck_len-2):
+    for j in range(i+1, balck_len-1):
+        for k in range(j+1, balck_len):
+            if(black[i] + black[j] + black[k] > m):
+                continue
+            else:
+                ans = max(ans ,black[i] + black[j] + black[k])
+
+print(ans)
