e489f5a04be018c571566a40152fdd1e19aa5e14
coolOlive <galilee155@naver.com>
2022-04-02 14:58:41 +0900

    220402_백준[2231], 파이썬

---
+++ 코딩테스트 공부/220402_백준[2231].py
@@ -0,0 +1,14 @@
+# 브루트 포스_분해합
+
+n=int(input())
+
+def divsum(n):
+    for i in range(1,n+1):
+        num=sum(map(int,str(i)))
+        tmp=num+i
+        if tmp==n:
+            return i
+        if i==n:
+            return 0
+
+print(divsum(n))
