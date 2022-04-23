bbbc4ab001b5aa9f065ff07fdddeeea9f3deee2d
coolOlive <galilee155@naver.com>
2022-04-23 20:25:47 +0900

    220423_백준[17298]/오큰수/파이썬

---
+++ 코딩테스트 공부/220423_백준[17298].py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+# 스택_오큰수/골드4
+
+n=int(input())
+num=list(map(int,input().split()))
+stack=[]
+ans=[-1]*n
+
+for i in range(n):
+    while stack and num[i]>num[stack[-1]]:
+        ans[stack.pop()]=num[i]
+    stack.append(i)
+        
+print(*ans)
