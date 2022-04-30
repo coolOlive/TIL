e376b6ec5dd810924ca9c4724369748a63ca455e
coolOlive <galilee155@naver.com>
2022-04-30 17:51:37 +0900

    220430_백준[1932]/정수 삼각형/ 파이썬

---
+++ 코딩테스트 공부/220430_백준[1932].py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+# DP_정수 삼각형/실버1
+
+n=int(input())
+dp=[list(map(int,input().split())) for _ in range(n)]
+
+
+for i in range(1,n):
+    dp[i][0]=dp[i][0]+dp[i-1][0]
+    dp[i][i]=dp[i][i]+dp[i-1][i-1]
+    for j in range(1,i):
+        dp[i][j]=max(dp[i-1][j-1],dp[i-1][j])+dp[i][j]
+        
+print(max(dp[n-1]))
