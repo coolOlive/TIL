fdd297da49055c6316f9e184f182cf9e368ca6c9
coolOlive <galilee155@naver.com>
2022-06-16 22:35:56 +0900

    220616_백준[1076]/저항/파이썬

---
+++ 코딩테스트 공부/220616_백준[1076].py
@@ -0,0 +1,10 @@
+# 구현_저항/브론즈2
+
+color = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+input1 = input()
+input2 = input()
+input3 = input()
+
+
+print(int(str(color.index(input1))+str(color.index(input2)))*(10**color.index(input3)))
