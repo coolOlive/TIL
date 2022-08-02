7236034c4210e15107a5118d7e2f8869fe79ea35
coolIlive <galilee155@naver.com>
2022-08-02 22:01:33 +0900

    220802_백준[1912]/연속합/dp/실버2

---
+++ 코딩테스트 공부/220802_백준[1912].py
@@ -0,0 +1,14 @@
+import sys
+input=sys.stdin.readline
+
+# dp_연속합/실버2
+
+n=int(input())
+nums=list(map(int,input().split()))
+
+dp=[0]*n
+dp[0]=nums[0]
+
+for i in range(1,n):
+    dp[i]=max(nums[i],dp[i-1]+nums[i])
+print(max(dp))
