6e32eeffabdaca2a71fcebc2bf4f7f110dac65e3
coolIlive <galilee155@naver.com>
2022-08-20 14:47:21 +0900

    220820_프로그래머스[핸드폰 번호 가리기]_L1/자바스크립트

---
+++ 코딩테스트 공부/220820_프로그래머스[핸드폰 번호 가리기]_L1.js
@@ -0,0 +1,12 @@
+function solution(phone_number) {
+    var answer = '';
+    
+    for (let i =0;i<phone_number.length-4;i++) {
+        answer+='*'
+    }
+    
+    for (let i =phone_number.length-4;i<phone_number.length;i++) {
+        answer+=String(phone_number[i])
+    }
+    return answer;
+}
