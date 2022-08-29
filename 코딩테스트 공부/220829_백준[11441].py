8a41b9102dd44e24ff4389c74c5ed611252fab29
coolIlive <galilee155@naver.com>
2022-08-29 23:12:09 +0900

    220829_백준[11441]/누적합/함 구하기/실버3/파이썬

---
+++ 코딩테스트 공부/220829_백준[11441].py
@@ -0,0 +1,18 @@
+import sys
+input=sys.stdin.readline
+
+ # 누적합/함 구하기/실버3
+
+n=int(input())
+num=list(map(int,input().split()))
+num_sum=[0]
+m=int(input())
+tmp=0
+
+for i in range(n):
+    tmp+=num[i]
+    num_sum.append(tmp)
+
+for _ in range(m):
+    a,b=map(int,input().split())
+    print(num_sum[b]-num_sum[a-1])
