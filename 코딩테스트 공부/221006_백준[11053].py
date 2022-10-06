c96956ff733f25988e6d8ae12cab54dde8f4c69a
coolIlive <galilee155@naver.com>
2022-10-06 11:49:06 +0900

    221006_백준[11053]/dp/가장 긴 증가하는 부분 수열/실버2/파이썬

---
+++ 코딩테스트 공부/221006_백준[11053].py
@@ -0,0 +1,15 @@
+import sys
+input=sys.stdin.readline
+
+ # dp/가장 긴 증가하는 부분 수열/실버2
+
+n=int(input())
+nums=list(map(int,input().split()))
+dp=[1 for _ in range(n)]
+
+for i in range(n):
+    for j in range(i):
+        if nums[i] > nums[j] and dp[i]<=dp[j]:
+            dp[i]=dp[j]+1
+
+print(max(dp))
