ae3d25b221b8e70613ec589a233388a457d79f07
coolIlive <galilee155@naver.com>
2022-12-13 23:27:17 +0900

    221213_백준[2303]/브루트포스/숫자 게임5/실버5/파이썬

---
+++ 코딩테스트 공부/221213_백준[2303].py
@@ -0,0 +1,17 @@
+from itertools import combinations
+import sys
+input=sys.stdin.readline
+
+# 브루트포스/숫자 게임5/실버5
+
+n = int(input())
+answer = []
+
+for i in range(n):
+    cards = list(combinations(list(map(int,input().split())),3))
+    score = 0
+    for num in cards:
+        score = max(score, sum(num)%10)
+    answer.append([score,i+1])
+
+print((max(answer)[1]))
