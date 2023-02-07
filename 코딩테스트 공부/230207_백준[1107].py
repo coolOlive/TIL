ec2774e8b8261b80d17bb378ba36dbac11c8ec49
coolIlive <galilee155@naver.com>
2023-02-07 13:59:25 +0900

    230207_백준[1107]/브루트포스/리모컨/골드5/파이썬

---
+++ 코딩테스트 공부/230207_백준[1107].py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+# 브루트포스/리모컨/골드5
+
+n = int(input())
+m = int(input())
+btn = list(map(str,input().split()))
+ans = abs(100-n)
+
+for i in range(1000001):
+    tmp = str(i)
+    for num in tmp:
+        if num in btn:
+            break
+    else:
+        ans = min(ans, abs(i - n) + len(tmp))
+
+print(ans)
