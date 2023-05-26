5bd21672173d04e9f13c2d50e42b7eb097b3cff2
coolIlive <galilee155@naver.com>
2023-05-26 22:13:15 +0900

    230526_백준[14494]/dp/다이나믹이 뭐예요?/실버3/파이썬

---
+++ 코딩테스트 공부/230526_백준[14494].py
@@ -0,0 +1,19 @@
+# dp/다이나믹이 뭐예요?/실버3
+# 시간 부족으로 검색해서 풀었다
+
+n, m = map(int, input().split())
+dp = [[0]*(m+1) for _ in range(n+1)]
+dp[1][1] = 1
+
+for y in range(1, n+1):
+  for x in range(1, m+1):
+    dx = x+1
+    dy = y+1
+    if 0<dx<=m:
+      dp[y][dx] += dp[y][x]
+    if 0<dy<=n:
+      dp[dy][x] += dp[y][x]
+    if 0<dx<=m and 0<dy<=n:
+      dp[dy][dx] += dp[y][x]
+
+print(dp[-1][-1]%1000000007)
