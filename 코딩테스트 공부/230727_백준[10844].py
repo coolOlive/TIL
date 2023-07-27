e7711bb4e8bb91a90918f5bb2cc724b4715657b5
coolIlive <galilee155@naver.com>
2023-07-27 20:54:09 +0900

    230727_백준[10844]/dp/쉬운 계단 수/실버1/파이썬

---
+++ 코딩테스트 공부/230727_백준[10844].py
@@ -0,0 +1,13 @@
+# dp/쉬운 계단 수/실버1
+
+n = int(input())
+dp = [[0] * 10 for _ in range(n+1)]
+dp[1] = [0,1,1,1,1,1,1,1,1,1]
+
+for i in range(2, n+1):
+  dp[i][0] = dp[i-1][1] 
+  dp[i][9] = dp[i-1][8] 
+  for j in range(1, 9):
+    dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
+
+print(sum(dp[n]) % 1000000000)
