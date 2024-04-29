132fb52628ecf9f5f07f48732c25d83fe3d5d307
coolIlive <galilee155@naver.com>
2024-04-29 23:33:25 +0900

    240429_백준[30455]/애드혹,개임이론/이제는 더 이상 물러날 곳이 없다/브론즈1/c++

---
+++ 코딩테스트 공부/240429_백준[30455].cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// 애드혹,개임이론/이제는 더 이상 물러날 곳이 없다/브론즈1
+
+int main(void)
+{
+  int n;
+  cin >> n;
+  if (n % 2 == 0)
+    cout << "Duck";
+  else
+    cout << "Goose";
+}
